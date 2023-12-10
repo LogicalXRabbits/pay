708520e0409d36659718699cb2a2e0a2d983929d
bolulembang <mpluto772@gmail.com>
2023-12-11 01:34:31 +0700

    first commit

---
+++ app/libs/product.js
@@ -0,0 +1,13 @@
+export const product = {
+    id: ~~(Math.random() * 100) + 1,
+    image: "/img.png",
+    name: "PAYMENT KAS KELAS TT0407",
+    price: 20000,
+    description: "jadi disin kalian tinggal menekan tombol paymen saja dan silahkan pilih payment yang kalian mau dari semua bang dan tombil - sama + itu adalah ketika kalian belum membayar kas sembelumnya jadi kalian tinggal menaikkan quantity nya saja..",
+    colors: [
+        { value: "putih", label: "Putih" },
+        { value: "biru", label: "Biru" },
+        { value: "coklat", label: "Coklat" },
+        { value: "kuning", label: "Kuning" }
+    ]
+}
